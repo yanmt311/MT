6300a73bc5eab8275c0a32b8df4820d631e0abe7
Ymenging_MT <yanmt311@163.com>
2020-03-17 13:08:53 +0800

    test

---
+++ 3.15/3.15/add.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+//定义额外全局变量
+//static int g_val = 2020;
+//
+// int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//
+//static int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
